b4ea72514f177f3c920f8f06cac2824f7e38f813
Jake Musich <makejusich@gmail.com>
2017-04-13 20:48:07 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,18 @@
-def hello_t
+## code provided didn't match the spec file
 
+def hello_t(names)
+    t_people = []
+    names.each do |name|
+      if name.start_with?("T") || name.start_with?("t")
+        yield(name)
+        t_people << name
+      end
+    end
+    t_people
 end
 
-# call your method here!
+  #block
 
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+    puts "Hi, #{name}"
+  end
